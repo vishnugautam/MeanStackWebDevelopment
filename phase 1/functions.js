88bf2167f284caf25036e63485e24bd22032732b
Vishnu Gautam <vishnugauham46@gmail.com>
2022-06-28 12:57:49 +0530

    removed the random edits

--- phase 1/functions.js
+++ phase 1/functions.js
@@ -1,7 +1,7 @@
 // 3 ways to declare the function 
 
 // user defined function - function statement
-function addition() {
+function addition(){
     var a = 6;
     var b = 5;
     var c = a + b;
@@ -12,7 +12,7 @@ addition(); // to call function directly
 
 // function constructor - it will create a new function object
 // it is used when we need to create many similar objects 
-function Add() { // if we have to return it as a 
+function Add(){ // if we have to return it as a 
     this.a = 5;
     this.b = 6;
     this.c = this.a + this.b;
@@ -21,7 +21,7 @@ function Add() { // if we have to return it as a
 // creating object of a function 
 var addedVal = new Add(); // new is used to create an new empty anonymous object
 var addedVal1 = new Add();
-addedVal.prototype.func = function () {
+addedVal.prototype.func = function(){
     console.log("The sum is " + this.c);
 }
 console.log(addedVal); // it creates an object like {a:5, b:6, c:11}
@@ -29,7 +29,7 @@ console.log(addedVal.c); // without new keyword it returns undefined as it doesn
 addedVal.func();
 
 // define a function inside an expression
-var addFunc = function () {
+var addFunc = function(){
     var a = 5;
     var b = 6;
     var c = a + b;
@@ -37,4 +37,3 @@ var addFunc = function () {
 };
 console.log(addFunc());
 
-// hello there
